cd68d5c5fe62672e2d753a97859fd2f3a09d8672
sanana14 <evanugapi14@gmail.com>
2025-01-01 06:56:38 -0800

    Update modul_satu_pbo.py

--- modul_satu_pbo.py
+++ modul_satu_pbo.py
@@ -13,3 +13,25 @@ class test:
 
 # silahkan lanjutkan dengan fungsi dan calss anda dibawah
 # pastikan untuk menguji class dan fungsi yang sudah di buat disini
+
+class Barang:
+    def __init__(self, nama, harga, diskon=0, pajak=0):
+        self.nama = nama
+        self.harga = harga
+        self.diskon = diskon
+        self.pajak = pajak
+
+    def harga_setelah_diskon(self):
+        """Menghitung harga setelah diskon"""
+        harga_diskon = self.harga * (self.diskon / 100)
+        return self.harga - harga_diskon
+
+    def harga_setelah_pajak(self):
+        """Menghitung harga setelah pajak"""
+        harga_diskon = self.harga_setelah_diskon()
+        harga_pajak = harga_diskon * (self.pajak / 100)
+        return harga_diskon + harga_pajak
+
+    def total_harga(self):
+        """Menghitung total harga setelah diskon dan pajak"""
+        return self.harga_setelah_pajak()
